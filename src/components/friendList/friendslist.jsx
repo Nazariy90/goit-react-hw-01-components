60ed95772e2a65079e5b06e70f2949011e0b4150
Nazariy90 <pinhvin123@gmail.com>
2023-04-25 15:54:46 +0300

    add

--- src/components/friendList/friendslist.jsx
+++ src/components/friendList/friendslist.jsx
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
-import "./FriendList.css";
+import css from "./FriendList.module.css";
 
 export function FriendList({ friends }) {
   return (
-    <ul>
+    <ul className={css.statisticsContainer}>
       {friends.map(({ id, isOnline, avatar, name }) => (
-        <li key={id} className="item">
-          <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
-          <img className="avatar" src={avatar} alt={`${name} avatar`} width="48" />
-          <p className="name">{name}</p>
+        <li key={id} className={css.item}>
+          <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
+          <img className={css.avatar} src={avatar} alt={`${name} avatar`} width="48" />
+          <p className={css.name}>{name}</p>
         </li>
       ))}
     </ul>
